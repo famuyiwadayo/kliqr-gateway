3c27eb2a040141cfd8d6a993a26bde4c072aca2f
Famuyiwa Dayo <famuyiwadayodaniel@gmail.com>
2021-04-23 09:31:04 +0100

    made the similar users query work independent of the trends input by the client

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { composeURL, createError } from "../utils";
-import { similarUsersDto, UserRo, UserTxCountRo } from "../interfaces";
+import { UserRo, UserTxCountRo } from "../interfaces";
 import { TransactionService } from ".";
 
 export default class UserService {
@@ -39,10 +39,10 @@ export default class UserService {
     }
   }
 
-  async getSimilarUsers(input: similarUsersDto): Promise<UserRo[]> {
-    console.log(input);
+  async getSimilarUsers(userId: number): Promise<UserRo[]> {
+    // console.log(input);
     try {
-      const userIds = await this.txService.getSimilarUsersId(input);
+      const userIds = await this.txService.getSimilarUsersId(userId);
       const toRunInParallel: any[] = [];
       userIds.forEach((id) => toRunInParallel.push(this.getUserById(id)));
       const result = await Promise.all(toRunInParallel);
